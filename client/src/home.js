74132ffd27e17e1d37f43d8809c7d6c2c3f00466
yuan830 <lmjhs980225@gmail.com>
2021-10-28 14:07:46 +0800

    1028 upload video

--- client/src/home.js
+++ client/src/home.js
@@ -9,18 +9,7 @@ class home extends React.Component {
             </div>
             <br/>
             <div className="home">
-            <iframe width="560" 
-            height="315" 
-            src="https://www.youtube.com/embed/D7TyOdOVlP4" 
-            title="YouTube video player" 
-            frameborder="0" 
-            allow="accelerometer; 
-            autoplay; 
-            clipboard-write; 
-            encrypted-media; 
-            gyroscope; 
-            picture-in-picture" allowfullscreen>
-            </iframe>
+            <iframe width="560" height="315" src="https://www.youtube.com/embed/eqMbqO4FEPs" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
             </div>
         </div>
     );
